021f5ce1fadc32c583171b48287976eacdbdc517
Kenta <kd1326756@st.kobedenshi.ac.jp>
2023-12-11 10:15:42 +0900

    20231211/10

--- script.js
+++ script.js
@@ -310,4 +310,15 @@ document.getElementById("imageFileInput").onchange = function (e) {
     reader.readAsDataURL(e.target.files[0]);
 }
 
+const sliderContainer = document.getElementById('canvas-container');
+const canvascontainer1 = document.getElementById('canvas');
+const canvascontainer2 = document.querySelector('.upper-canvas');
+const zoomSlider = document.getElementById('zoom-slidebar');
+
+zoomSlider.addEventListener('input', function () {
+    const sliderScale = zoomSlider.value;
+    canvascontainer1.style.transform = `scale(${sliderScale})`;
+    canvascontainer2.style.transform = `scale(${sliderScale})`;
+});
+
 resetSettings();
